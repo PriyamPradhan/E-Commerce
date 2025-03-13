e57c0a8b3a41245e238ba6bdc5af30f3d3dbad77
Priyam Pradhan <priyampradhan07@gmail.com>
2025-03-13 21:55:44 +0530

    added payment dto

---
+++ src/main/java/com/priyamdev/ecom/dto/Payment.java
@@ -0,0 +1,46 @@
+package com.priyamdev.ecom.dto;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Column(precision = 10, scale = 2)
+    private Double amount;
+
+    @Column(nullable = false)
+    private String paymentStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @Column(name = "valid_until")
+    private Date validUntil;
+
+    public enum PaymentMethod {
+        CREDIT_CARD,
+        DEBIT_CARD,
+        PAYPAL,
+        BANK_TRANSFER,
+        CASH_ON_DELIVERY,
+        CRYPTOCURRENCY
+    }
+
+}
